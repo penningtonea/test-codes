d165a584d78f18313c9d7fd42da0abb06b1f2489
penningtonea <70979519+penningtonea@users.noreply.github.com>
2023-05-26 15:11:55 -0400

    Adding note section with use directions on the top

--- scraper.py
+++ scraper.py
@@ -1,9 +1,11 @@
 #%%
-
+'''
+This is a model script needed to search and retrieve a list of PMIDs relating to a user-specified gene list. The a new gene list should be added for each new org or subspecies researched.
+'''
 from Bio import Entrez
 
 Entrez.email = 'epennington@gwmail.gwu.edu'
-sEntericaGeneList = ['parE', 'parC', 'gyrA', 'gyrB', 'soxR','csgA','hilA','ihfA','invA','invE','phoP','sopE','spvC','rrd 16s rRNA']
+sEntericaGeneList = ['parE', 'parC', 'gyrA', 'gyrB', 'soxR','csgA','hilA','ihfA','invA','invE','phoP','sopE','spvC','rrd 16s rRNA', 'tet']
 
 pmidList =[]
 
@@ -17,3 +19,11 @@ print("Salmonella enterica PMIDs:\n",pmidList)
 
 # %%
 
+from Bio import Entrez
+
+Entrez.email = 'epennington@gwmail.gwu.edu'
+handle = Entrez.esearch(db='pubmed', term=('Salmonella enterica[title]'+ 'substitution[title]' + 'ramR'))
+record = Entrez.read(handle)
+print("Salmonella enterica PMIDs:\n",record['IdList'])
+
+# %%
